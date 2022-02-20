0a24262694a66b3762985a41f75b5b78c235c452
Vasileios Anagnostopoulos <anagnwstopoulos@hotmail.com>
2022-02-20 23:15:25 +0100

    Fixing the premature de-allocation of the bcm2835

--- src/Service/impl/PiFanController.cpp
+++ src/Service/impl/PiFanController.cpp
@@ -8,7 +8,7 @@
 
 namespace {
 
-constexpr int MAX_FAN = 480;
+constexpr uint32_t MAX_FAN = 480;
 
 }// namespace
 
@@ -17,30 +17,51 @@ namespace PiFan {
 
 PiFanController::PiFanController() : m_pwm_level{ std::numeric_limits<unsigned int>::min() }
 {
+    if (m_counter == 0) {
 #ifdef MOCK_BCM2835
-    bcm2835_set_debug(1);
+        bcm2835_set_debug(1);
 #endif
-    if (!bcm2835_init()) throw std::runtime_error("Unable to initialize bcm2835");
+        if (!bcm2835_init()) throw std::runtime_error("Unable to initialize bcm2835");
 
-    // two exposed GPIO pins are capable of hardware PWM (GPIO 13 and GPIO 18)
-    // We use them both, so the user can connect his fan to any of these two pins
-    bcm2835_gpio_fsel(13, BCM2835_GPIO_FSEL_ALT0);
-    bcm2835_gpio_fsel(18, BCM2835_GPIO_FSEL_ALT5);
+        // two exposed GPIO pins are capable of hardware PWM (GPIO 13 and GPIO 18)
+        // We use them both, so the user can connect his fan to any of these two pins
+        bcm2835_gpio_fsel(13, BCM2835_GPIO_FSEL_ALT0);
+        bcm2835_gpio_fsel(18, BCM2835_GPIO_FSEL_ALT5);
 
-    bcm2835_pwm_set_clock(BCM2835_PWM_CLOCK_DIVIDER_2);
-    bcm2835_pwm_set_mode(1, 1, 1);
-    bcm2835_pwm_set_range(1, MAX_FAN);
+        bcm2835_pwm_set_clock(BCM2835_PWM_CLOCK_DIVIDER_2);
+        bcm2835_pwm_set_mode(1, 1, 1);
+        bcm2835_pwm_set_range(1, MAX_FAN);
 
-    bcm2835_pwm_set_mode(0, 1, 1);
-    bcm2835_pwm_set_range(0, MAX_FAN);
-    setSpeed(FanThrottlePercent(0));
+        bcm2835_pwm_set_mode(0, 1, 1);
+        bcm2835_pwm_set_range(0, MAX_FAN);
+        setSpeed(FanThrottlePercent(0));
+    }
+    ++m_counter;
 }
 
-PiFanController::~PiFanController() noexcept { bcm2835_close(); }
+PiFanController::PiFanController(PiFanController &&) noexcept
+{
+    ++m_counter;
+}
+
+PiFanController & PiFanController::operator=(PiFanController &&) noexcept
+{
+    ++m_counter;
+    return *this;
+}
+
+PiFanController::~PiFanController() noexcept
+{
+    --m_counter;
+    if(0 == m_counter)
+    {
+        bcm2835_close();
+    }
+}
 
 void PiFanController::setSpeed(FanThrottlePercent throttle)
 {
-    auto level = static_cast<unsigned int>(throttle.number() / 100 * MAX_FAN);
+    auto level = static_cast<uint32_t>(throttle.number() / 100 * MAX_FAN);
     if (level > MAX_FAN) level = MAX_FAN;
 
     if (level > m_pwm_level && (level - m_pwm_level) < 5) return;
--- src/Service/include/PiFan/Service/PiFanController.h
+++ src/Service/include/PiFan/Service/PiFanController.h
@@ -3,6 +3,7 @@
 #include <units/generic/dimensionless.h>
 
 #include <vector>
+#include <atomic>
 
 #include "degree_celsius.h"
 #include "service_export.h"
@@ -20,14 +21,19 @@ public:
 
     PiFanController(const PiFanController &other) noexcept = delete;
     PiFanController &operator=(const PiFanController &other) noexcept = delete;
-    PiFanController(PiFanController &&other) noexcept = default;
-    PiFanController &operator=(PiFanController &&other) noexcept = default;
+    PiFanController(PiFanController &&other) noexcept;
+    PiFanController &operator=(PiFanController &&other) noexcept;
 
 
     void setSpeed(FanThrottlePercent throttle);
 
 private:
     unsigned int m_pwm_level = 0;
+
+    // The initialization and de-initialization has to happen once.
+    // So as long as we have still an instance alive, we should not
+    // close the bcm2835 connection
+    inline static std::atomic<int> m_counter = 0;
 };
 
 }// namespace PiFan
